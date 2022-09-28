9f018b4710b4c740560ec381957909d21b27dad7
VeredMazor <veredm582@gmail.com>
2022-09-28 14:46:40 +0300

    28/9

--- DeltaPredictClient/App.js
+++ DeltaPredictClient/App.js
@@ -21,6 +21,7 @@ export default function App() {
       
     </Stack.Navigator>
   </NavigationContainer>
+    
   );
 }    
 //hey
--- DeltaPredictClient/Views/Welcome.js
+++ DeltaPredictClient/Views/Welcome.js
@@ -1,16 +1,17 @@
 import React, { useEffect,useState ,TouchableOpacity} from "react";
-import {StyleSheet, Text, View, TextInput, Button, Image, ImageBackground} from "react-native";
+import {StyleSheet, Text, View, TextInput, Button, Image, ImageBackground, Platform } from "react-native";
 import { useNavigation } from '@react-navigation/native';
+
 import SignUp from "./SignUp";
 import { color } from "react-native-reanimated";
 
 
 //check if login details are correct in DB
 function _onPressButton (username,password,navigation) {
-    fetch('http://localhost:5000/authenticate', {
+    fetch('http://192.168.1.22:5000/authenticate', {
         method: 'POST', 
         headers: { 'Content-Type': 'application/json' }, 
-        body: JSON.stringify({  name: username,Password:password })
+        body: JSON.stringify({name: username, Password: password})
     })
     .then(res => res.json())
     .then(data => { data.result === "true" ? navigation.navigate('Dashboard') : alert("wrong details") });
@@ -25,18 +26,69 @@ export default function App({  })  {
     const navigation = useNavigation();
     
     return (
-    <View style={styles.container}>
-        <View style={styles.backImage} >
-         <Image
-            source={require('../assets/icon.png')}
-            style={{ flex: 1 }}
-            resizeMode="contain"
-    />      </View>
-       
+        <View style={styles.container}>
+            <View style={styles.backImage} >
+            <Image
+                source={require('../assets/icon.png')}
+                style={{ flex: 1 }}
+                resizeMode="contain" />
+            </View>
+                <View style={styles.blackScreen}>
+                    <View style={styles.featuredDetails} > 
+                    {PickerOS()}
+                    </View>
 
-        <View style={styles.blackScreen}>
 
-        <View style={styles.featuredDetails}>
+
+                    <View style={styles.columnContainer}>
+                        <Text style={styles.baseText}> Login </Text>
+                            <View style={styles.inputView}>
+                            <TextInput
+                            style={styles.TextInput}
+                            placeholder="Email."
+                            placeholderTextColor="#fff"
+                            onChangeText={(email) => setEmail(email)}
+                            />
+                            </View>
+
+                            <View style={styles.inputView}>
+                            <TextInput
+                            style={styles.TextInput}
+                            placeholder="Password."
+                            placeholderTextColor="#fff"
+                            secureTextEntry={true}
+                            onChangeText={(password) => setPassword(password)}
+                            />
+                            </View>
+                    </View>
+                    
+                
+                </View>
+                <View style={styles.btnSignUp}>
+                <Button title ="Sign Up" color = "#1e222d"
+                onPress={() => navigation.navigate('SignUp')}
+                />
+                </View>
+                <View style={styles.btnStart}>
+                <Button title = "Start!" color = "#01a37b"
+                onPress={() => _onPressButton(email,password,navigation)}
+                />
+                </View>
+                
+        </View>
+                
+    
+    );
+
+}
+
+const PickerOS = () => {
+    return Platform.OS === "android" ? (
+        <Text >
+           
+        
+        </Text>
+    ) : (
         <Text style={styles.descriptionText}>Delta predict is a stock market prediction app. 
         Stock forecasts can be a helpful tool for making investment decisions. They can help you:{'\n'}dentify undervalued and overvalued stocks: when a stock's price is not in line with its fundamentals, it may be undervalued {'\n'}
         or overvalued. Why buy an overvalued stock when you can buy an undervalued stock for less? This is where stock price targets can be helpful.
@@ -45,46 +97,12 @@ export default function App({  })  {
         With so many stocks to choose from, it can be challenging to know which ones are worth investing in.{'\n'}
         A stock forecast can help you narrow down your options and make more informed investment decisions.
         </Text>
-        </View>
-
-        <View style={styles.columnContainer}>
-        <Text style={styles.baseText}> Login </Text>
-        <View style={styles.inputView}>
-            <TextInput
-            style={styles.TextInput}
-            placeholder="Email."
-            placeholderTextColor="#fff"
-            
-            onChangeText={(email) => setEmail(email)}
-            />
-        </View>
-        <View style={styles.inputView}>
-        <TextInput
-            style={styles.TextInput}
-            placeholder="Password."
-            placeholderTextColor="#fff"
-            secureTextEntry={true}
-            onChangeText={(password) => setPassword(password)}
-            />
-        </View>
-        </View>
-        
-        </View>
-        <View style={styles.btnSignUp}>
-        <Button title ="Sign Up" color = "#1e222d"
-        onPress={() => navigation.navigate('SignUp')}
-        />
-    </View>
-    <View style={styles.btnStart}>
-        <Button title = "Start!" color = "#01a37b"
-        onPress={() => _onPressButton(email,password,navigation)}
-    />
-    </View>
-    </View>
-    
     );
+  };
 
-}
+
+
+
 
 //secureTextEntry={true}
 const styles = StyleSheet.create({
@@ -92,6 +110,10 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#131722",
         justifyContent: 'flex-start',
+        // ...Platform.select({
+        //     android: {backgroundColor: '#FFFFFF',},
+
+        // })
     },
     blackScreen: {
         marginTop: 75,
@@ -108,13 +130,25 @@ const styles = StyleSheet.create({
         marginBottom: 5,
         marginLeft: 225,
         flexDirection: "column",
+        ...Platform.select({
+            android: {marginLeft: 40, width: 230, height: 300,},
+
+        })
+       
     },
     baseText: {
-        color: '#5B738B',
+        color: '#40B5AD',
+        textShadowColor: 'black', 
+        textShadowOffset: { width: -1, height: 0 },
+        textShadowRadius: 10,
         margin: 10,
         marginLeft: 135,
         fontSize: 22,
         fontWeight: 'bold',
+        ...Platform.select({
+            android: {marginLeft: 75,},
+
+        })
     },
     inputView: {
         backgroundColor: "#131722",
@@ -122,6 +156,7 @@ const styles = StyleSheet.create({
         marginTop: 20,
         borderWidth: 2,
         borderColor: '#50535e',
+       
     },
     screenContainer: {
         flex: 1,
@@ -137,7 +172,11 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
         borderColor: "grey",
-        color: 'white',
+        color: 'white', 
+        ...Platform.select({
+            android: { color: 'white',flex: 0, height: 34, fontSize: 12,},
+
+        })
     },
     loginBtn: {
         width: "30%",
@@ -159,12 +198,15 @@ const styles = StyleSheet.create({
     featuredDetails: {
         flexDirection: "row",
         backgroundColor: "#1e222d",
-        //position: "s",
         flexDirection: "row",
         marginLeft: 25,
         marginVertical: 40,
         marginTop: 50,
         marginBottom: 200,
+         ...Platform.select({
+            android: {backgroundColor: '#1e222d', marginHorizontal: 20, marginTop: 80,},
+
+        })
     },
     appButtonContainer: {
         elevation: 8,
@@ -188,8 +230,11 @@ const styles = StyleSheet.create({
         marginTop: -210,
         marginLeft: 1500,
         marginRight: 50, 
-        alignItems: "left",
-        color: "#131722",
+        color: "#1e222d",
+        ...Platform.select({
+            android: {width: 80, height: 80 , marginLeft: 235, marginTop: -130,},
+
+        })
     },
     btnStart:{
         width: "10%",
@@ -197,12 +242,21 @@ const styles = StyleSheet.create({
         marginTop: 10,
         marginLeft: 1330,
         marginRight: 50,  
-        alignItems: "left",
+        
+        ...Platform.select({
+            android: {width: 130, height: 80 , marginLeft: 135, marginTop: -30,},
+
+        })
     },
     backImage: {
         width: 330,
         height: 150,
         marginLeft: 22,
+
+        ...Platform.select({
+            android: {width: 5, height: 60, marginLeft: 8, marginTop: 50,},
+
+        })
     },
 
 });
